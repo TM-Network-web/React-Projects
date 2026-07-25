16c140471f7f3a96f7d9f0242db3f552fb6a0c1a
Muhammad Tayyab <tayyabjabbareb@gmail.com>
2026-07-25 23:32:09 +0500

    Add Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='p-4 flex border-t-2 border-orange-400/30 justify-center items-center'>
+      <p>&copy; 2026 Muhammad Tayyab. All Rights Reserved.</p>
+    </footer>
   )
 }
 
